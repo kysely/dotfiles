bec3db130acc9eec146b238d4452f6ff4eaf7f09
Radek Kysely <kyselyradek@gmail.com>
2018-07-08 17:34:26 +0200

    Added `better-windmove`

--- emacs/init.el
+++ emacs/init.el
@@ -35,6 +35,32 @@
   (while (member (buffer-name) skippable-buffers)
     (next-buffer)))
 
+(defvar bw-directions-list
+  '((?h windmove-left)
+    (?j windmove-down)
+    (?k windmove-up)
+    (?l windmove-right))
+  "List of mappings between the key and corresponding windmove action.")
+
+(defun bw-func (char)
+  "Return the correct windmove action based on provided CHAR from `bw-directions-list`."
+  (nth 1 (assoc char bw-directions-list)))
+
+(defun better-windmove (dir)
+  "Vim-like window navigation using hjkl keys.
+
+To completely mimic Vim behavior, bind `better-windmove` to C-w.
+Note that `evil-mode` already does this, however `better-windmove`
+can be still used throughout all Vim modes and on a different binding.
+
+      (global-set-key (kbd \"C-w\") 'better-windmove)
+  "
+  (interactive "cDirection (h/j/k/l)?")
+  (let ((action (bw-func dir)))
+    (if (not (eq action nil))
+        (funcall action)
+        (user-error "Direction not recognised"))))
+
 ;; ENSURE ALL PACKAGES
 (use-package exec-path-from-shell
   :ensure t
