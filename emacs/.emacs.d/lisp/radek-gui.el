a9abef6119d2e9b8f528eb8f902bb82cdcf83ed6
Radek Kysely <kyselyradek@gmail.com>
2019-01-05 01:50:02 +0100

    Completely new Monokai theme; updated Powerline

--- emacs/.emacs.d/lisp/radek-gui.el
+++ emacs/.emacs.d/lisp/radek-gui.el
@@ -1,14 +1,25 @@
 (use-package monokai-theme              ; Default theme
  :ensure t
  :config
- (setq
-  monokai-foreground     "#F2F3F7"
-  monokai-background     "#27292C"
-  monokai-highlight      "#4D4F52"
-  monokai-comments       "#595C61"
-  monokai-highlight-line "#2E3133"
-  monokai-line-number    "#595C61"
-  monokai-gray           "#2E3133")
+ (setq ;; foreground and background
+       monokai-foreground     "#FCFCFA"
+       monokai-background     "#2D2A2E"
+       ;; highlights and comments
+       monokai-comments       "#5B595C"
+       monokai-emphasis       "#D9D9D7"
+       monokai-highlight      "#434144"
+       monokai-highlight-alt  "#66D9EF" ; Default
+       monokai-highlight-line "#383539"
+       monokai-line-number    "#5B595C"
+       ;; colours
+       monokai-blue           "#78DCE8"
+       monokai-cyan           "#78DCE8"
+       monokai-green          "#A9DC76"
+       monokai-gray           "#353236"
+       monokai-violet         "#AB9DF2"
+       monokai-red            "#FF6188"
+       monokai-orange         "#FC9867"
+       monokai-yellow         "#FFD866")
  (load-theme 'monokai t))
 
 (use-package dash-functional            ; Dependency for pretty-fonts & -eshell
@@ -80,79 +91,63 @@
 
 ;; MODE LINE (style & layout) --------------------------------------------------
 (defface my-pl-gui-style
-  '((t (:foreground "#595C61" :background "#2E3033" :box nil)))
+  '((t (:foreground "#787875" :background "#353236" :box nil)))
   "My custom GUI powerline face")
 (defface my-pl-active-gui-style
-  '((t (:foreground "#F2F3F7" :background "#2E3033" :box nil)))
+  '((t (:foreground "#FCFCFA" :background "#3C393D" :box nil)))
   "My custom GUI powerline face for the first segment")
-
-(defface my-pl-term-style
-  '((t (:foreground "#5E5E59" :background "#303030" :box nil)))
-  "My custom terminal powerline face")
-(defface my-pl-active-term-style
-  '((t (:foreground "#BDB9B1" :background "#303030" :box nil)))
-  "My custom terminal powerline face for the first segment")
 
 (defun minimal-powerline-theme ()
   "Set up my custom Powerline with Evil indicators."
   (setq powerline-height 30)
-  (setq powerline-display-hud nil)
-  (setq powerline-default-separator (if (display-graphic-p) 'box nil))
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (gui (display-graphic-p))
-                          (seg1
-                            (if gui
-                              (if active 'my-pl-active-gui-style 'my-pl-gui-style)
-                              (if active 'my-pl-active-term-style 'my-pl-term-style)))
-                          (seg2 (if gui 'my-pl-gui-style 'my-pl-term-style))
-                          (seg3 (if gui 'my-pl-gui-style 'my-pl-term-style))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (let ((evil-face (powerline-evil-face)))
-                                       (if evil-mode
-                                           (powerline-raw (powerline-evil-tag) evil-face)
-                                         ))
-                                     (if evil-mode
-                                         (funcall separator-left (powerline-evil-face) seg1))
-                                     (powerline-buffer-id seg1 'l)
-                                     (powerline-raw "[%*]" seg1 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format seg1 'l))
-                                     (powerline-raw " " seg1)
-                                     (funcall separator-left seg1 seg2)
-                                     (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object seg2 'l))
-                                     (powerline-major-mode seg2 'l)
-                                     (powerline-process seg2)
-                                     (powerline-minor-modes seg2 'l)
-                                     (powerline-narrow seg2 'l)
-                                     (powerline-raw " " seg2)
-                                     (funcall separator-left seg2 seg3)
-                                     (when (bound-and-true-p nyan-mode)
-                                       (powerline-raw (list (nyan-create)) seg3 'l))))
-                          (rhs (list (powerline-raw global-mode-string seg3 'r)
-                                     (powerline-vc seg3 'r)
-                                     (funcall separator-right seg3 seg2)
-                                     (unless window-system
-                                       (powerline-raw (char-to-string #xe0a1) seg2 'l))
-                                     (powerline-raw "%4l" seg2 'l)
-                                     (powerline-raw ":" seg2 'l)
-                                     (powerline-raw "%3c" seg2 'r)
-                                     (funcall separator-right seg2 seg1)
-                                     (powerline-raw " " seg1)
-                                     (powerline-raw "%6p" seg1 'r)
-                                     (when powerline-display-hud
-                                       (powerline-hud seg1 seg3)))))
-                     (concat (powerline-render lhs)
-                             (powerline-fill seg3 (powerline-width rhs))
-                             (powerline-render rhs)))))))
+  (setq powerline-display-hud t)
+  (setq powerline-default-separator nil)
+  (setq powerline-gui-use-vcs-glyph t)
+  (setq-default
+   mode-line-format
+   '("%e"
+     (:eval
+      (let* ((active (powerline-selected-window-active))
+             (seg1 (if active 'my-pl-active-gui-style 'my-pl-gui-style))
+             (seg2 'my-pl-gui-style)
+             (seg3 'my-pl-gui-style)
+             (separator-left (intern (format "powerline-%s-%s"
+                                             (powerline-current-separator)
+                                             (car powerline-default-separator-dir))))
+             (separator-right (intern (format "powerline-%s-%s"
+                                              (powerline-current-separator)
+                                              (cdr powerline-default-separator-dir))))
+             (lhs (list (let ((evil-face (powerline-evil-face)))
+                          (if evil-mode
+                              (powerline-raw (powerline-evil-tag) evil-face)
+                            ))
+                        (if evil-mode
+                            (funcall separator-left (powerline-evil-face) seg1))
+                        (powerline-buffer-id seg1 'l)
+                        (powerline-raw "[%*]" seg1 'l)
+                        (when (and (boundp 'which-func-mode) which-func-mode)
+                          (powerline-raw which-func-format seg1 'l))
+                        (powerline-raw " " seg1)
+                        (funcall separator-left seg1 seg2)
+                        (when (boundp 'erc-modified-channels-object)
+                          (powerline-raw erc-modified-channels-object seg2 'l))
+                        (powerline-major-mode seg2 'l)
+                        (powerline-process seg2)
+                        (powerline-narrow seg2 'l)
+                        (powerline-raw " " seg2)))
+             (rhs (list (powerline-raw global-mode-string seg2 'r)
+                        (powerline-vc seg2 'r)
+		    (funcall separator-right seg2 seg1)
+		    (unless window-system
+                          (powerline-raw (char-to-string #xe0a1) seg1 'l))
+                        (powerline-raw "%4l" seg1 'l)
+                        (powerline-raw ":" seg1 'l)
+                        (powerline-raw "%3c" seg1 'r)
+                        (when powerline-display-hud
+                          (powerline-hud seg1 seg2)))))
+        (concat (powerline-render lhs)
+                (powerline-fill seg3 (powerline-width rhs))
+                (powerline-render rhs)))))))
 
 (use-package powerline
   :ensure t
