58d58c1c7e4193148619d044072514c08053303c
Radek Kysely <radek@kysely.org>
2019-06-18 19:36:14 +0200

    Emacs: more minimal look

--- emacs/.emacs.d/lisp/radek-gui.el
+++ emacs/.emacs.d/lisp/radek-gui.el
@@ -1,31 +1,7 @@
-(use-package monokai-theme              ; Default theme
- :ensure t
- :config
- (setq ;; foreground and background
-       monokai-foreground     "#FCFCFA"
-       monokai-background     "#2D2A2E"
-       ;; highlights and comments
-       monokai-comments       "#5B595C"
-       monokai-emphasis       "#D9D9D7"
-       monokai-highlight      "#434144"
-       monokai-highlight-alt  "#66D9EF" ; Default
-       monokai-highlight-line "#383539"
-       monokai-line-number    "#5B595C"
-       ;; colours
-       monokai-blue           "#78DCE8"
-       monokai-cyan           "#78DCE8"
-       monokai-green          "#A9DC76"
-       monokai-gray           "#353236"
-       monokai-violet         "#AB9DF2"
-       monokai-red            "#FF6188"
-       monokai-orange         "#FC9867"
-       monokai-yellow         "#FFD866")
- (load-theme 'monokai t))
-
-(use-package rainbow-identifiers        ; Semantic syntax highlighting
+(use-package kaolin-themes              ; Default theme
   :ensure t
   :config
-  (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
+  (load-theme 'kaolin-ocean t))
 
 (use-package dimmer                     ; Dim inactive buffers
   :ensure t
@@ -37,10 +13,10 @@
   :ensure t)
 
 ;; FONT STYLE ------------------------------------------------------------------
-(setq-default line-spacing 6)
+(setq-default line-spacing 7)
 (set-face-attribute 'default nil
                     :family "Fira Code"
-                    :height 130
+                    :height 100
                     :weight 'light)
 
 (use-package pretty-fonts
@@ -100,74 +76,9 @@
 
     (setq pretty-eshell-funcs (list esh-dir esh-git esh-clock esh-num))))
 
-;; MODE LINE (style & layout) --------------------------------------------------
-(defface my-pl-gui-style
-  '((t (:foreground "#787875" :background "#353236" :box nil)))
-  "My custom GUI powerline face")
-(defface my-pl-active-gui-style
-  '((t (:foreground "#FCFCFA" :background "#3C393D" :box nil)))
-  "My custom GUI powerline face for the first segment")
-
-(defun minimal-powerline-theme ()
-  "Set up my custom Powerline with Evil indicators."
-  (setq powerline-height 30)
-  (setq powerline-display-hud t)
-  (setq powerline-default-separator nil)
-  (setq powerline-gui-use-vcs-glyph t)
-  (setq-default
-   mode-line-format
-   '("%e"
-     (:eval
-      (let* ((active (powerline-selected-window-active))
-             (seg1 (if active 'my-pl-active-gui-style 'my-pl-gui-style))
-             (seg2 'my-pl-gui-style)
-             (seg3 'my-pl-gui-style)
-             (separator-left (intern (format "powerline-%s-%s"
-                                             (powerline-current-separator)
-                                             (car powerline-default-separator-dir))))
-             (separator-right (intern (format "powerline-%s-%s"
-                                              (powerline-current-separator)
-                                              (cdr powerline-default-separator-dir))))
-             (lhs (list (let ((evil-face (powerline-evil-face)))
-                          (if evil-mode
-                              (powerline-raw (powerline-evil-tag) evil-face)
-                            ))
-                        (if evil-mode
-                            (funcall separator-left (powerline-evil-face) seg1))
-                        (powerline-buffer-id seg1 'l)
-                        (powerline-raw "[%*]" seg1 'l)
-                        (when (and (boundp 'which-func-mode) which-func-mode)
-                          (powerline-raw which-func-format seg1 'l))
-                        (powerline-raw " " seg1)
-                        (funcall separator-left seg1 seg2)
-                        (when (boundp 'erc-modified-channels-object)
-                          (powerline-raw erc-modified-channels-object seg2 'l))
-                        (powerline-major-mode seg2 'l)
-                        (powerline-process seg2)
-                        (powerline-narrow seg2 'l)
-                        (powerline-raw " " seg2)))
-             (rhs (list (powerline-raw global-mode-string seg2 'r)
-                        (powerline-vc seg2 'r)
-		    (funcall separator-right seg2 seg1)
-		    (unless window-system
-                          (powerline-raw (char-to-string #xe0a1) seg1 'l))
-                        (powerline-raw "%4l" seg1 'l)
-                        (powerline-raw ":" seg1 'l)
-                        (powerline-raw "%3c" seg1 'r)
-                        (when powerline-display-hud
-                          (powerline-hud seg1 seg2)))))
-        (concat (powerline-render lhs)
-                (powerline-fill seg3 (powerline-width rhs))
-                (powerline-render rhs)))))))
-
-(use-package powerline
+;; MODE LINE ----------------------------------------------------------------
+(use-package doom-modeline
   :ensure t
-  :config
-
-  (use-package powerline-evil
-    :ensure t)
-
-  (minimal-powerline-theme))
-
+  :hook (after-init . doom-modeline-mode))
 
 (provide 'radek-gui)
