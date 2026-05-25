a0de461ab9e078fc9ec79473e2da1640b17061af
Radek Kyselý <radek@kysely.org>
2026-05-25 18:21:38 +0200

    feat: Add Flexoki Light theme for Blink shell (#1)
    * add Flexoki Light theme for Blink SH
    
    * make the theme a bit more vibrant

---
+++ blink/flexoki-light.js
@@ -0,0 +1,25 @@
+// Flexoki Light theme for Blink Shell
+// Palette: https://stephango.com/flexoki
+
+t.prefs_.set('color-palette-overrides', [
+  '#100F0F', // black
+  '#C03E35', // red
+  '#768D21', // green
+  '#BE9207', // yellow
+  '#3171B2', // blue
+  '#B74583', // magenta
+  '#2F968D', // cyan
+  '#DAD8CE', // white
+  '#878580', // bright black
+  '#D14D41', // bright red
+  '#879A39', // bright green
+  '#D0A215', // bright yellow
+  '#4385BE', // bright blue
+  '#CE5D97', // bright magenta
+  '#3AA99F', // bright cyan
+  '#F2F0E5'  // bright white
+]);
+
+t.prefs_.set('foreground-color', '#100F0F');
+t.prefs_.set('background-color', '#FFFCF0');
+t.prefs_.set('cursor-color', 'rgba(64,62,60,0.5)');
